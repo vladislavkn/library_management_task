259d2273c220067b299b6860e436f98af7977fc4
Vladislav Knyshov <inova136@gmail.com>
2025-04-12 18:48:36 +0200

    feat: add PostgreSQL initialization scripts and update Docker Compose for data persistence

---
+++ sql_init/01-schema.sql
@@ -0,0 +1,214 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.8
+-- Dumped by pg_dump version 16.8
+
+-- Started on 2025-04-12 16:42:51 UTC
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE IF EXISTS library_db;
+--
+-- TOC entry 3415 (class 1262 OID 16384)
+-- Name: library_db; Type: DATABASE; Schema: -; Owner: library_user
+--
+
+CREATE DATABASE library_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE library_db OWNER TO library_user;
+
+\connect library_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 217 (class 1259 OID 16397)
+-- Name: book; Type: TABLE; Schema: public; Owner: library_user
+--
+
+CREATE TABLE public.book (
+    book_id integer NOT NULL,
+    title character varying(1024) NOT NULL,
+    author character varying(1024),
+    place character varying(1024),
+    quantity integer NOT NULL,
+    publisher character varying(1024),
+    genre character varying(1024),
+    CONSTRAINT book_quantity_check CHECK ((quantity >= 0))
+);
+
+
+ALTER TABLE public.book OWNER TO library_user;
+
+--
+-- TOC entry 216 (class 1259 OID 16396)
+-- Name: book_book_id_seq; Type: SEQUENCE; Schema: public; Owner: library_user
+--
+
+CREATE SEQUENCE public.book_book_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.book_book_id_seq OWNER TO library_user;
+
+--
+-- TOC entry 3416 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: book_book_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: library_user
+--
+
+ALTER SEQUENCE public.book_book_id_seq OWNED BY public.book.book_id;
+
+
+--
+-- TOC entry 219 (class 1259 OID 16407)
+-- Name: borrow; Type: TABLE; Schema: public; Owner: library_user
+--
+
+CREATE TABLE public.borrow (
+    borrow_id integer NOT NULL,
+    book_id integer NOT NULL,
+    borrower_email character varying(255) NOT NULL,
+    start_date date NOT NULL,
+    return_date date,
+    is_returned boolean DEFAULT false
+);
+
+
+ALTER TABLE public.borrow OWNER TO library_user;
+
+--
+-- TOC entry 218 (class 1259 OID 16406)
+-- Name: borrow_borrow_id_seq; Type: SEQUENCE; Schema: public; Owner: library_user
+--
+
+CREATE SEQUENCE public.borrow_borrow_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.borrow_borrow_id_seq OWNER TO library_user;
+
+--
+-- TOC entry 3417 (class 0 OID 0)
+-- Dependencies: 218
+-- Name: borrow_borrow_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: library_user
+--
+
+ALTER SEQUENCE public.borrow_borrow_id_seq OWNED BY public.borrow.borrow_id;
+
+
+--
+-- TOC entry 215 (class 1259 OID 16389)
+-- Name: borrower; Type: TABLE; Schema: public; Owner: library_user
+--
+
+CREATE TABLE public.borrower (
+    email character varying(255) NOT NULL,
+    password_hash text NOT NULL
+);
+
+
+ALTER TABLE public.borrower OWNER TO library_user;
+
+--
+-- TOC entry 3255 (class 2604 OID 16400)
+-- Name: book book_id; Type: DEFAULT; Schema: public; Owner: library_user
+--
+
+ALTER TABLE ONLY public.book ALTER COLUMN book_id SET DEFAULT nextval('public.book_book_id_seq'::regclass);
+
+
+--
+-- TOC entry 3256 (class 2604 OID 16410)
+-- Name: borrow borrow_id; Type: DEFAULT; Schema: public; Owner: library_user
+--
+
+ALTER TABLE ONLY public.borrow ALTER COLUMN borrow_id SET DEFAULT nextval('public.borrow_borrow_id_seq'::regclass);
+
+
+--
+-- TOC entry 3262 (class 2606 OID 16405)
+-- Name: book book_pkey; Type: CONSTRAINT; Schema: public; Owner: library_user
+--
+
+ALTER TABLE ONLY public.book
+    ADD CONSTRAINT book_pkey PRIMARY KEY (book_id);
+
+
+--
+-- TOC entry 3264 (class 2606 OID 16413)
+-- Name: borrow borrow_pkey; Type: CONSTRAINT; Schema: public; Owner: library_user
+--
+
+ALTER TABLE ONLY public.borrow
+    ADD CONSTRAINT borrow_pkey PRIMARY KEY (borrow_id);
+
+
+--
+-- TOC entry 3260 (class 2606 OID 16395)
+-- Name: borrower borrower_pkey; Type: CONSTRAINT; Schema: public; Owner: library_user
+--
+
+ALTER TABLE ONLY public.borrower
+    ADD CONSTRAINT borrower_pkey PRIMARY KEY (email);
+
+
+--
+-- TOC entry 3265 (class 2606 OID 16414)
+-- Name: borrow borrow_book_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: library_user
+--
+
+ALTER TABLE ONLY public.borrow
+    ADD CONSTRAINT borrow_book_id_fkey FOREIGN KEY (book_id) REFERENCES public.book(book_id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 3266 (class 2606 OID 16419)
+-- Name: borrow borrow_borrower_email_fkey; Type: FK CONSTRAINT; Schema: public; Owner: library_user
+--
+
+ALTER TABLE ONLY public.borrow
+    ADD CONSTRAINT borrow_borrower_email_fkey FOREIGN KEY (borrower_email) REFERENCES public.borrower(email) ON DELETE CASCADE;
+
+
+-- Completed on 2025-04-12 16:42:51 UTC
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ sql_init/02-data.sql
@@ -0,0 +1,92 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.8
+-- Dumped by pg_dump version 16.8
+
+-- Started on 2025-04-12 16:44:09 UTC
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 3408 (class 0 OID 16397)
+-- Dependencies: 217
+-- Data for Name: book; Type: TABLE DATA; Schema: public; Owner: library_user
+--
+
+COPY public.book (book_id, title, author, place, quantity, publisher, genre) FROM stdin;
+1	The People of the Abyss	Jack London	Shelf A1	5	Macmillan	Non-fiction
+2	The Last Wish	Andrzej Sapkowski	Shelf B2	3	SuperNOWA	Fantasy
+3	Cloud Atlas	David Mitchell	Shelf C3	4	Sceptre	Speculative Fiction
+4	Brave New World	Aldous Huxley	Shelf D4	6	Chatto & Windus	Dystopian
+5	Nightfall	Isaac Asimov	Shelf E1	2	Street & Smith	Science Fiction
+6	Berserk	Kentaro Miura	Shelf F5	10	Hakusensha	Manga
+7	Northern Lights (The Golden Compass)	Philip Pullman	Shelf G3	3	Scholastic	Fantasy
+8	The Great Gatsby	F. Scott Fitzgerald	Shelf H2	4	Scribner	Classic
+9	The Last Samurai	Helen DeWitt	Shelf I6	1	Talk Miramax Books	Literary Fiction
+10	The Dream Room	Erich Maria Remarque	Shelf J1	2	Atlantic Books	Drama
+\.
+
+
+--
+-- TOC entry 3406 (class 0 OID 16389)
+-- Dependencies: 215
+-- Data for Name: borrower; Type: TABLE DATA; Schema: public; Owner: library_user
+--
+
+COPY public.borrower (email, password_hash) FROM stdin;
+alice@example.com	hash1
+bob@example.com	hash2
+carol@example.com	hash3
+\.
+
+
+--
+-- TOC entry 3410 (class 0 OID 16407)
+-- Dependencies: 219
+-- Data for Name: borrow; Type: TABLE DATA; Schema: public; Owner: library_user
+--
+
+COPY public.borrow (borrow_id, book_id, borrower_email, start_date, return_date, is_returned) FROM stdin;
+1	1	alice@example.com	2025-04-01	2025-04-10	t
+2	2	alice@example.com	2025-04-05	\N	f
+3	3	bob@example.com	2025-03-28	2025-04-03	t
+4	4	carol@example.com	2025-04-07	\N	f
+5	9	bob@example.com	2025-04-10	\N	f
+\.
+
+
+--
+-- TOC entry 3416 (class 0 OID 0)
+-- Dependencies: 216
+-- Name: book_book_id_seq; Type: SEQUENCE SET; Schema: public; Owner: library_user
+--
+
+SELECT pg_catalog.setval('public.book_book_id_seq', 10, true);
+
+
+--
+-- TOC entry 3417 (class 0 OID 0)
+-- Dependencies: 218
+-- Name: borrow_borrow_id_seq; Type: SEQUENCE SET; Schema: public; Owner: library_user
+--
+
+SELECT pg_catalog.setval('public.borrow_borrow_id_seq', 5, true);
+
+
+-- Completed on 2025-04-12 16:44:09 UTC
+
+--
+-- PostgreSQL database dump complete
+--
+
